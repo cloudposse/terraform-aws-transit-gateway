1fbfdaf4c0186e3b441e1d5a29faeb1947ad5a09
aknysh <andriy.knysh@gmail.com>
2020-07-15 23:32:26 -0400

    Initial commit

--- main.tf
+++ main.tf
@@ -1,11 +1,52 @@
-resource "random_integer" "example" {
-  min = 1
-  max = 50000
-  keepers = {
-    example = var.example
-  }
+locals {
+  enabled_count = var.enabled ? 1 : 0
 }
 
-locals {
-  example = format("%v %d", var.example, random_integer.example.result)
+module "base_label" {
+  source      = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.16.0"
+  namespace   = var.namespace
+  name        = var.name
+  stage       = var.stage
+  environment = var.environment
+  delimiter   = var.delimiter
+  attributes  = var.attributes
+  tags        = var.tags
+  enabled     = var.enabled
+}
+
+module "tgw_label" {
+  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.16.0"
+  context    = module.base_label.context
+  attributes = distinct(compact(concat(module.base_label.attributes, ["tgw"])))
+}
+
+resource "aws_ec2_transit_gateway" "default" {
+  count                           = local.enabled_count
+  description                     = "AWS Transit Gateway"
+  auto_accept_shared_attachments  = var.auto_accept
+  default_route_table_association = var.default_route_table_association
+  default_route_table_propagation = var.default_route_table_propagation
+  tags                            = module.tgw_label.tags
+}
+
+# Add a Blackhole route to the transit gateway
+resource "aws_ec2_transit_gateway_route" "tgw_blackhole_route" {
+  count                          = var.add_blackhole_route && var.enabled ? 1 : 0
+  destination_cidr_block         = "0.0.0.0/0"
+  blackhole                      = true
+  transit_gateway_route_table_id = join("", aws_ec2_transit_gateway.default.*.association_default_route_table_id)
+}
+
+# Setup RAM for transit Gateway
+module "ram_label" {
+  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.16.0"
+  context    = module.base_label.context
+  attributes = distinct(compact(concat(module.base_label.attributes, ["ram"])))
+}
+
+resource "aws_ram_resource_share" "tgw_ram_share" {
+  count                     = local.enabled_count
+  name                      = "example"
+  allow_external_principals = var.allow_external_principles
+  tags                      = module.ram_label.tags
 }
--- variables.tf
+++ variables.tf
@@ -1,4 +1,79 @@
-variable "example" {
-  description = "Example variable"
-  default     = "hello world"
+variable "enabled" {
+  type        = bool
+  default     = true
+  description = "Set to false to prevent the module from creating or accessing any resources"
+}
+
+variable "namespace" {
+  description = "Namespace (e.g. `eg` or `cp`)"
+  type        = string
+}
+
+variable "environment" {
+  type        = string
+  default     = ""
+  description = "Environment, e.g. 'prod', 'staging', 'dev', 'pre-prod', 'UAT'"
+}
+
+variable "stage" {
+  description = "Stage (e.g. `prod`, `dev`, `staging`)"
+  type        = string
+}
+
+variable "name" {
+  description = "Name  (e.g. `app` or `cluster`)"
+  type        = string
+}
+
+variable "delimiter" {
+  type        = string
+  default     = "-"
+  description = "Delimiter to be used between `namespace`, `stage`, `name`, and `attributes`"
+}
+
+variable "attributes" {
+  type        = list(string)
+  default     = []
+  description = "Additional attributes (e.g. `a` or `b`)"
+}
+
+variable "tags" {
+  type        = map(string)
+  default     = {}
+  description = "Additional tags (e.g. `{\"BusinessUnit\" = \"XYZ\"`)"
+}
+
+variable "auto_accept" {
+  type        = "string"
+  default     = "disable"
+  description = "Whether resource attachment requests are automatically accepted. Valid values: disable, enable. Default value: disable."
+}
+
+variable "default_route_table_association" {
+  type        = "string"
+  default     = "enable"
+  description = "Whether resource attachments are automatically associated with the default association route table. Valid values: disable, enable. Default value: enable."
+}
+
+variable "default_route_table_propagation" {
+  type        = "string"
+  default     = "enable"
+  description = "Whether resource attachments automatically propagate routes to the default propagation route table. Valid values: disable, enable. Default value: enable."
+}
+
+variable "add_blackhole_route" {
+  type        = bool
+  default     = true
+  description = "Whether or not to add a blackhole route for 0.0.0.0/0 to the transit gateway, default is true"
+}
+
+variable "allow_external_principles" {
+  type        = bool
+  default     = false
+  description = "Indicates whether principals outside your organization can be associated with a resource share."
+}
+variable "allowed_principles" {
+  type        = list(string)
+  default     = []
+  description = "List of principles allowed to access this share, can be account id's or organzation arn's"
 }
