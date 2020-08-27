abad9a88448ef5985254fa2d88be14898abc5e65
aknysh <andriy.knysh@gmail.com>
2020-08-27 17:55:22 -0400

    Add context

--- main.tf
+++ main.tf
@@ -1,54 +1,55 @@
 locals {
-  enabled_count = var.enabled ? 1 : 0
-}
-
-module "base_label" {
-  source      = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.19.0"
-  enabled     = var.enabled
-  namespace   = var.namespace
-  name        = var.name
-  stage       = var.stage
-  environment = var.environment
-  delimiter   = var.delimiter
-  attributes  = var.attributes
-  tags        = var.tags
-}
-
-module "tgw_label" {
-  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.19.0"
-  enabled    = var.enabled
-  context    = module.base_label.context
-  attributes = distinct(compact(concat(module.base_label.attributes, ["tgw"])))
+  enabled_count = module.this.enabled ? 1 : 0
 }
 
 resource "aws_ec2_transit_gateway" "default" {
   count                           = local.enabled_count
-  description                     = "AWS Transit Gateway"
+  description                     = "${module.this.id} Transit Gateway"
   auto_accept_shared_attachments  = var.auto_accept
   default_route_table_association = var.default_route_table_association
   default_route_table_propagation = var.default_route_table_propagation
-  tags                            = module.tgw_label.tags
+  dns_support                     = "enable"
+  vpn_ecmp_support                = "enable"
+  tags                            = module.this.tags
+}
+
+# https://www.terraform.io/docs/providers/aws/r/ec2_transit_gateway_route_table.html
+resource "aws_ec2_transit_gateway_route_table" "default" {
+  count              = local.enabled_count
+  transit_gateway_id = join("", aws_ec2_transit_gateway.default.*.id)
+  tags               = module.this.tags
 }
 
 # Add a Blackhole route to the transit gateway
 resource "aws_ec2_transit_gateway_route" "tgw_blackhole_route" {
-  count                          = var.add_blackhole_route && var.enabled ? 1 : 0
+  count                          = var.add_blackhole_route ? local.enabled_count : 0
   destination_cidr_block         = "0.0.0.0/0"
   blackhole                      = true
-  transit_gateway_route_table_id = join("", aws_ec2_transit_gateway.default.*.association_default_route_table_id)
+  transit_gateway_route_table_id = join("", aws_ec2_transit_gateway_route_table.default.*.id)
 }
 
-# Setup RAM for transit Gateway
-module "ram_label" {
-  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.19.0"
-  enabled    = var.enabled
-  context    = module.base_label.context
-  attributes = distinct(compact(concat(module.base_label.attributes, ["ram"])))
-}
-
-resource "aws_ram_resource_share" "tgw_ram_share" {
+# RAM share for transit Gateway
+resource "aws_ram_resource_share" "default" {
   count                     = local.enabled_count
-  name                      = "example"
-  allow_external_principals = var.allow_external_principles
-  tags                      = module.ram_label.tags
+  name                      = module.this.id
+  allow_external_principals = var.allow_external_principals
+  tags                      = module.this.tags
+}
+
+# Share the Transit Gateway with the Organization
+# https://www.terraform.io/docs/providers/aws/d/organizations_organization.html
+data "aws_organizations_organization" "default" {}
+
+# https://www.terraform.io/docs/providers/aws/r/ram_resource_association.html
+resource "aws_ram_resource_association" "default" {
+  count              = local.enabled_count
+  resource_arn       = join("", aws_ec2_transit_gateway.default.*.arn)
+  resource_share_arn = join("", aws_ram_resource_share.default.*.id)
+}
+
+# https://www.terraform.io/docs/providers/aws/r/ram_principal_association.html
+resource "aws_ram_principal_association" "default" {
+  count              = local.enabled_count
+  principal          = data.aws_organizations_organization.default.arn
+  resource_share_arn = join("", aws_ram_resource_share.default.*.id)
 }
--- variables.tf
+++ variables.tf
@@ -1,34 +1,34 @@
 variable "auto_accept" {
-  type        = "string"
-  default     = "disable"
-  description = "Whether resource attachment requests are automatically accepted. Valid values: disable, enable. Default value: disable."
+  type        = string
+  default     = "enable"
+  description = "Whether resource attachment requests are automatically accepted. Valid values: `disable`, `enable`. Default value: `disable`"
 }
 
 variable "default_route_table_association" {
-  type        = "string"
-  default     = "enable"
-  description = "Whether resource attachments are automatically associated with the default association route table. Valid values: disable, enable. Default value: enable."
+  type        = string
+  default     = "disable"
+  description = "Whether resource attachments are automatically associated with the default association route table. Valid values: `disable`, `enable`. Default value: `enable`"
 }
 
 variable "default_route_table_propagation" {
-  type        = "string"
-  default     = "enable"
-  description = "Whether resource attachments automatically propagate routes to the default propagation route table. Valid values: disable, enable. Default value: enable."
+  type        = string
+  default     = "disable"
+  description = "Whether resource attachments automatically propagate routes to the default propagation route table. Valid values: `disable`, `enable`. Default value: `enable`"
 }
 
 variable "add_blackhole_route" {
   type        = bool
   default     = true
-  description = "Whether or not to add a blackhole route for 0.0.0.0/0 to the transit gateway, default is true"
+  description = "Whether or not to add a blackhole route for 0.0.0.0/0 to the Transit Gateway, default is `true`"
 }
 
-variable "allow_external_principles" {
+variable "allow_external_principals" {
   type        = bool
   default     = false
-  description = "Indicates whether principals outside your organization can be associated with a resource share."
+  description = "Indicates whether principals outside your organization can be associated with a resource share"
 }
 variable "allowed_principles" {
   type        = list(string)
   default     = []
-  description = "List of principles allowed to access this share, can be account id's or organzation arn's"
+  description = "List of principals allowed to access this share, can be account IDs or Organization ARNs"
 }
