ebe9ab2370cc860b2f0596122d18036f5a1cf7fe
aknysh <andriy.knysh@gmail.com>
2020-09-07 21:05:57 -0400

    Updates

--- examples/multi-account/main.tf
+++ examples/multi-account/main.tf
@@ -1,17 +1,18 @@
 # Create the Transit Gateway, route table associations/propagations, and static TGW routes in the `network` account.
-# Enable sharing the Transit Gateway with the Organization using Resource Access Manager (RAM)
+# Enable sharing the Transit Gateway with the Organization using Resource Access Manager (RAM).
 # If you would like to share resources with your organization or organizational units,
 # then you must use the AWS RAM console or CLI command to enable sharing with AWS Organizations.
 # When you share resources within your organization,
-# AWS RAM does not send invitations to principals. Principals in your organization get access to shared resources without exchanging invitations
+# AWS RAM does not send invitations to principals. Principals in your organization get access to shared resources without exchanging invitations.
 # https://docs.aws.amazon.com/ram/latest/userguide/getting-started-sharing.html
 
 module "transit_gateway" {
   source = "../../"
 
+  ram_resource_share_enabled = true
+
   create_transit_gateway                                         = true
   create_transit_gateway_route_table                             = true
-  ram_resource_share_enabled                                     = true
   create_transit_gateway_vpc_attachment                          = false
   create_transit_gateway_route_table_association_and_propagation = true
 
