7a7e22a6fbe1b92faae887be220e0c54a29cc1fb
Maxim Mironenko <maxim@cloudposse.com>
2021-01-04 16:54:39 +0700

    Terraform 0.14 upgrade

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -3,7 +3,8 @@ provider "aws" {
 }
 
 module "vpc_prod" {
-  source     = "git::https://github.com/cloudposse/terraform-aws-vpc.git?ref=tags/0.17.0"
+  source     = "cloudposse/vpc/aws"
+  version    = "0.17.0"
   cidr_block = "172.16.0.0/16"
 
   attributes = ["prod"]
@@ -11,7 +12,8 @@ module "vpc_prod" {
 }
 
 module "subnets_prod" {
-  source                  = "git::https://github.com/cloudposse/terraform-aws-dynamic-subnets.git?ref=tags/0.28.0"
+  source                  = "cloudposse/dynamic-subnets/aws"
+  version                 = "0.28.0"
   availability_zones      = var.availability_zones
   vpc_id                  = module.vpc_prod.vpc_id
   igw_id                  = module.vpc_prod.igw_id
@@ -25,7 +27,8 @@ module "subnets_prod" {
 }
 
 module "vpc_staging" {
-  source     = "git::https://github.com/cloudposse/terraform-aws-vpc.git?ref=tags/0.17.0"
+  source     = "cloudposse/vpc/aws"
+  version    = "0.17.0"
   cidr_block = "172.32.0.0/16"
 
   attributes = ["staging"]
@@ -33,7 +36,8 @@ module "vpc_staging" {
 }
 
 module "subnets_staging" {
-  source                  = "git::https://github.com/cloudposse/terraform-aws-dynamic-subnets.git?ref=tags/0.28.0"
+  source                  = "cloudposse/dynamic-subnets/aws"
+  version                 = "0.28.0"
   availability_zones      = var.availability_zones
   vpc_id                  = module.vpc_staging.vpc_id
   igw_id                  = module.vpc_staging.igw_id
@@ -47,7 +51,8 @@ module "subnets_staging" {
 }
 
 module "vpc_dev" {
-  source     = "git::https://github.com/cloudposse/terraform-aws-vpc.git?ref=tags/0.17.0"
+  source     = "cloudposse/vpc/aws"
+  version    = "0.17.0"
   cidr_block = "172.48.0.0/16"
 
   attributes = ["dev"]
@@ -55,7 +60,8 @@ module "vpc_dev" {
 }
 
 module "subnets_dev" {
-  source                  = "git::https://github.com/cloudposse/terraform-aws-dynamic-subnets.git?ref=tags/0.28.0"
+  source                  = "cloudposse/dynamic-subnets/aws"
+  version                 = "0.28.0"
   availability_zones      = var.availability_zones
   vpc_id                  = module.vpc_dev.vpc_id
   igw_id                  = module.vpc_dev.igw_id
