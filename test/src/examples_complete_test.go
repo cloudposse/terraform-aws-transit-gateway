b18f3d69b4b388226d606d84de298ec669106df3
Paul Robinson <paul.robinson@internetfusion.co.uk>
2021-07-26 19:24:59 +0100

    Add test for context disabled
    * Test fails since root module doesn't inspect the context to see if it's enabled
    * Disabled parallel tests since the init phase module download fails
    * Add fixtures to make it clear what the test variables are
    * Force the enabled = true for the base vpc infrastructure in the test module

--- test/src/examples_complete_test.go
+++ test/src/examples_complete_test.go
@@ -11,24 +11,13 @@ import (
 
 // Test the Terraform module in examples/complete using Terratest.
 func TestExamplesComplete(t *testing.T) {
-	t.Parallel()
+	// Init phase module download fails when run in parallel
+	//t.Parallel()
 
 	rand.Seed(time.Now().UnixNano())
 
 	randId := strconv.Itoa(rand.Intn(100000))
 	attributes := []string{randId}
-
-	terraformVpcOptions := &terraform.Options{
-		// The path to where our Terraform code is located
-		TerraformDir: "../../examples/complete",
-		Upgrade:      true,
-		// Variables to pass to our Terraform code using -var-file options
-		VarFiles: []string{"fixtures.us-east-2.tfvars"},
-		Vars: map[string]interface{}{
-			"attributes": attributes,
-		},
-		Targets: []string{"module.vpc_prod", "module.subnets_prod", "module.vpc_staging", "module.subnets_staging", "module.vpc_dev", "module.subnets_dev"},
-	}
 
 	terraformOptions := &terraform.Options{
 		// The path to where our Terraform code is located
@@ -41,13 +30,20 @@ func TestExamplesComplete(t *testing.T) {
 		},
 		Targets: []string{"module.transit_gateway"},
 	}
+	terraformVpcOptions := &terraform.Options{
+		TerraformDir: terraformOptions.TerraformDir,
+		Upgrade:      terraformOptions.Upgrade,
+		// We always want the VPC base infrastructure to be enabled. Also forced in the code.
+		VarFiles: []string{"fixtures.us-east-2.tfvars", "fixtures.enabled.tfvars"},
+		Vars:     terraformOptions.Vars,
+		Targets:  []string{"module.vpc_prod", "module.subnets_prod", "module.vpc_staging", "module.subnets_staging", "module.vpc_dev", "module.subnets_dev"},
+	}
 
 	// At the end of the test, run `terraform destroy` to clean up any resources that were created
 	defer func() {
 		terraformOptions.Targets = []string{"module.transit_gateway"}
 		terraform.Destroy(t, terraformOptions)
-		terraformOptions.Targets = []string{"module.vpc_prod", "module.subnets_prod", "module.vpc_staging", "module.subnets_staging", "module.vpc_dev", "module.subnets_dev"}
-		terraform.Destroy(t, terraformOptions)
+		terraform.Destroy(t, terraformVpcOptions)
 	}()
 
 	// This will run `terraform init` and `terraform apply` and fail the test if there are any errors
@@ -78,4 +74,56 @@ func TestExamplesComplete(t *testing.T) {
 	transitGatewayRouteIds := terraform.OutputMap(t, terraformOptions, "transit_gateway_route_ids")
 	// Verify we're getting back the outputs we expect
 	assert.Equal(t, 3, len(transitGatewayRouteIds))
+}
+
+func TestExamplesCompleteDisabledModule(t *testing.T) {
+	// Init phase module download fails when run in parallel
+	//t.Parallel()
+
+	rand.Seed(time.Now().UnixNano())
+
+	randId := strconv.Itoa(rand.Intn(100000))
+	attributes := []string{randId}
+
+	terraformOptions := &terraform.Options{
+		// The path to where our Terraform code is located
+		TerraformDir: "../../examples/complete",
+		Upgrade:      true,
+		// Variables to pass to our Terraform code using -var-file options
+		VarFiles: []string{"fixtures.us-east-2.tfvars", "fixtures.disabled.tfvars"},
+		Vars: map[string]interface{}{
+			"attributes": attributes,
+		},
+	}
+	terraformVpcOptions := &terraform.Options{
+		TerraformDir: terraformOptions.TerraformDir,
+		Upgrade:      terraformOptions.Upgrade,
+		// We always want the VPC base infrastructure to be enabled. Also forced in the code.
+		VarFiles: []string{"fixtures.us-east-2.tfvars", "fixtures.enabled.tfvars"},
+		Vars:     terraformOptions.Vars,
+		Targets:  []string{"module.vpc_prod", "module.subnets_prod", "module.vpc_staging", "module.subnets_staging", "module.vpc_dev", "module.subnets_dev"},
+	}
+
+	// At the end of the test, run `terraform destroy` to clean up any resources that were created
+	defer func() {
+		terraformOptions.Targets = []string{"module.transit_gateway"}
+		terraform.Destroy(t, terraformOptions)
+		terraform.Destroy(t, terraformVpcOptions)
+	}()
+
+	// This will run `terraform init` and `terraform apply` and fail the test if there are any errors
+	terraform.InitAndApply(t, terraformVpcOptions)
+	terraform.Apply(t, terraformOptions)
+
+	transitGatewayArn := terraform.Output(t, terraformOptions, "transit_gateway_arn")
+	transitGatewayRouteTableId := terraform.Output(t, terraformOptions, "transit_gateway_route_table_id")
+	transitGatewayVpcAttachmentIds := terraform.OutputMap(t, terraformOptions, "transit_gateway_vpc_attachment_ids")
+	subnetRouteIds := terraform.OutputMap(t, terraformOptions, "subnet_route_ids")
+	transitGatewayRouteIds := terraform.OutputMap(t, terraformOptions, "transit_gateway_route_ids")
+
+	assert.Empty(t, transitGatewayArn)
+	assert.Empty(t, transitGatewayRouteTableId)
+	assert.Empty(t, transitGatewayVpcAttachmentIds)
+	assert.Empty(t, subnetRouteIds)
+	assert.Empty(t, transitGatewayRouteIds)
 }
