291c47be54c6cae650d67fedc929c2305900d05d
nitrocode <7775707+nitrocode@users.noreply.github.com>
2022-05-16 22:49:17 -0400

    Use v4.4.0 and rm local and random

--- examples/complete/versions.tf
+++ examples/complete/versions.tf
@@ -2,17 +2,9 @@ terraform {
   required_version = ">= 0.13.0"
 
   required_providers {
-    local = {
-      source  = "hashicorp/local"
-      version = ">= 1.2"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = ">= 2.2"
-    }
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.0"
+      version = ">= 4.4.0"
     }
   }
 }
--- versions.tf
+++ versions.tf
@@ -2,14 +2,6 @@ terraform {
   required_version = ">= 0.13.0"
 
   required_providers {
-    local = {
-      source  = "hashicorp/local"
-      version = ">= 1.2"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = ">= 2.2"
-    }
     aws = {
       source  = "hashicorp/aws"
       version = ">= 4.4.0"
