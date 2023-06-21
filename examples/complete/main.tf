0a253764f9ae86bd9020cc5ac1d9f34945e49a77
Max Lobur <max_lobur@outlook.com>
2023-06-21 17:54:10 +0300

    updates

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -19,8 +19,8 @@ module "subnets_prod" {
 
   availability_zones      = var.availability_zones
   vpc_id                  = module.vpc_prod.vpc_id
-  igw_id                  = module.vpc_prod.igw_id
-  ipv4_cidr_block         = module.vpc_prod.vpc_cidr_block
+  igw_id                  = [module.vpc_prod.igw_id]
+  ipv4_cidr_block         = [module.vpc_prod.vpc_cidr_block]
   nat_gateway_enabled     = false
   nat_instance_enabled    = false
   map_public_ip_on_launch = false
@@ -47,8 +47,8 @@ module "subnets_staging" {
 
   availability_zones      = var.availability_zones
   vpc_id                  = module.vpc_staging.vpc_id
-  igw_id                  = module.vpc_staging.igw_id
-  ipv4_cidr_block         = module.vpc_staging.vpc_cidr_block
+  igw_id                  = [module.vpc_staging.igw_id]
+  ipv4_cidr_block         = [module.vpc_staging.vpc_cidr_block]
   nat_gateway_enabled     = false
   nat_instance_enabled    = false
   map_public_ip_on_launch = false
@@ -75,8 +75,8 @@ module "subnets_dev" {
 
   availability_zones      = var.availability_zones
   vpc_id                  = module.vpc_dev.vpc_id
-  igw_id                  = module.vpc_dev.igw_id
-  ipv4_cidr_block         = module.vpc_dev.vpc_cidr_block
+  igw_id                  = [module.vpc_dev.igw_id]
+  ipv4_cidr_block         = [module.vpc_dev.vpc_cidr_block]
   nat_gateway_enabled     = false
   nat_instance_enabled    = false
   map_public_ip_on_launch = false
