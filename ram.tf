af87a66770546462f3e2a6e228d30e229ef76b79
RoseSecurity <72598486+RoseSecurity@users.noreply.github.com>
2025-09-22 11:41:05 -0400

    refactor: use local.ram_resource_share_enabled consistently (#65)
    Replace direct references to var.ram_resource_share_enabled with the new
    local variable local.ram_resource_share_enabled throughout the code to
    ensure consistent behavior and improve maintainability.

--- ram.tf
+++ ram.tf
@@ -1,6 +1,8 @@
 locals {
+  ram_resource_share_enabled = module.this.enabled && var.ram_resource_share_enabled
+
   ram_principals_provided = var.ram_principal != null || length(var.ram_principals) > 0
-  ram_principals = toset(var.ram_resource_share_enabled ? toset(
+  ram_principals = toset(local.ram_resource_share_enabled ? toset(
     local.ram_principals_provided ? concat(
       var.ram_principal == null ? [] : [var.ram_principal],
       var.ram_principals,
@@ -13,7 +15,7 @@ locals {
 # Resource Access Manager (RAM) share for the Transit Gateway
 # https://docs.aws.amazon.com/ram/latest/userguide/what-is.html
 resource "aws_ram_resource_share" "default" {
-  count                     = var.ram_resource_share_enabled ? 1 : 0
+  count                     = local.ram_resource_share_enabled ? 1 : 0
   name                      = module.this.id
   allow_external_principals = var.allow_external_principals
   tags                      = module.this.tags
@@ -21,11 +23,11 @@ resource "aws_ram_resource_share" "default" {
 
 # Share the Transit Gateway with the Organization if RAM principal was not provided
 data "aws_organizations_organization" "default" {
-  count = var.ram_resource_share_enabled && !local.ram_principals_provided ? 1 : 0
+  count = local.ram_resource_share_enabled && !local.ram_principals_provided ? 1 : 0
 }
 
 resource "aws_ram_resource_association" "default" {
-  count              = var.ram_resource_share_enabled ? 1 : 0
+  count              = local.ram_resource_share_enabled ? 1 : 0
   resource_arn       = try(aws_ec2_transit_gateway.default[0].arn, "")
   resource_share_arn = try(aws_ram_resource_share.default[0].id, "")
 }
